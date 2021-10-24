24071144ddf2b2640195ce8b6f5f2e62fc7304ff
Pfunzo Mudau <pfunzomudau@outlook.com>
2021-10-25 00:33:51 +0200

    home page added on package.json

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
@@ -21,17 +21,124 @@ const responsive = {
     items: 2,
   },
 };
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
+const GItems = (props) => {
+  return props.data ? (
+    props.data.map((d, i) => (
+      <div key={`${d.id}-${i}`} className="gallery-item">
+        <div className="deal-card">
+          <div className={"card-body"}>
+            <img src={d.image} className="card-img-top" alt="..." />
+            <p className="card-text">{`${d.title}`}</p>
+          </div>
+        </div>
+      </div>
+    ))
+  ) : (
+    <div></div>
+  );
 };
 export const Services = (props) => {
-  const [currentSlide, setCurrentSlide] = useState(3);
+  // super(props);
+  const [galleryContainer, setGalleryContainer] = useState({});
+  const [galleryControls, setGalleryConrols] = useState(["Previous", "Next"]);
+  const [galleryControlsContainer, setgalleryControlsContainer] = useState({});
+  const [newDirection, setNewDirection] = useState("next");
+  const [directionNext, setDirectionNext] = useState(0);
+  const [directionPrevious, setDirectionPrevious] = useState(0);
+  //carouselContainer = {};
+  //carouselControls = {};
+  const [count, setCount] = useState(0);
+  const [carouselArray, setcarouselArray] = useState([]);
+  useEffect(() => {
+    const galleryItems = document.querySelectorAll(".gallery-item");
+    if (galleryItems) {
+      if (count == 0) setcarouselArray((arry) => (arry = [...galleryItems]));
+      if (galleryItems.length > 0) {
+        if (count == 0) {
+          setCount(galleryItems.length);
+        }
+      }
+    }
+  }, [carouselArray]);
+  useEffect(() => {
+    if (carouselArray) {
+      if (carouselArray.length > 0) {
+        setcarouselArray((arr) => {
+          // arr[0].classList.add("gallery-item-first")
+          arr[0].classList.add("gallery-item-first");
+          arr[1].classList.add("gallery-item-previous");
+          arr[2].classList.add("gallery-item-selected");
+          arr[3].classList.add("gallery-item-next");
+          arr[4].classList.add("gallery-item-last");
+        });
+      }
+    }
+    setgalleryControlsContainer(document.querySelector(".gallery-controls"));
+    //console.log(carouselArray);
+  }, [count]);
+
+  useEffect(() => {
+    //const triggers = [...galleryControlsContainer.childNodes];
+
+    //const target = control;
+    const selected = document.querySelectorAll(".gallery-item-selected");
+    const previous = document.querySelectorAll(".gallery-item-previous");
+    const next = document.querySelectorAll(".gallery-item-next");
+    const first = document.querySelectorAll(".gallery-item-first");
+    const last = document.querySelectorAll(".gallery-item-last");
+    selected.forEach((el) => {
+      el.classList.remove("gallery-item-selected");
+
+      if (newDirection == "previous") {
+        el.classList.add("gallery-item-next");
+      } else {
+        el.classList.add("gallery-item-previous");
+      }
+    });
+
+    previous.forEach((el) => {
+      el.classList.remove("gallery-item-previous");
+
+      if (newDirection == "previous") {
+        el.classList.add("gallery-item-selected");
+      } else {
+        el.classList.add("gallery-item-first");
+      }
+    });
+
+    next.forEach((el) => {
+      el.classList.remove("gallery-item-next");
+
+      if (newDirection == "previous") {
+        el.classList.add("gallery-item-last");
+      } else {
+        el.classList.add("gallery-item-selected");
+      }
+    });
+
+    first.forEach((el) => {
+      el.classList.remove("gallery-item-first");
+
+      if (newDirection == "previous") {
+        el.classList.add("gallery-item-previous");
+      } else {
+        el.classList.add("gallery-item-last");
+      }
+    });
+
+    last.forEach((el) => {
+      el.classList.remove("gallery-item-last");
+
+      if (newDirection == "previous") {
+        el.classList.add("gallery-item-first");
+      } else {
+        el.classList.add("gallery-item-next");
+      }
+    });
+  }, [directionNext, directionPrevious, newDirection]);
+
   return (
-    <div id="services" className="text-center">
+    <div id="services">
       <div className="container">
         <div className="section-title">
           <p>
@@ -39,40 +146,32 @@ export const Services = (props) => {
             <span className="bold-span"> here to do?</span>
           </p>
         </div>
+        <div className="row gallery-container">
+          <GItems data={props.data} />
+        </div>
+      </div>
+      <div className="container">
         <div className="row">
-          <Carousel
-            swipeable={false}
-            responsive={responsive}
-            ssr={true} // means to render carousel on server-side.
-            infinite={true}
-            customTransition="all .5"
-            containerClass="carousel-container"
-            removeArrowOnDeviceType={["tablet", "mobile"]}
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px"
-            afterChange={(nextSlide) => setCurrentSlide(nextSlide - 1)}
-          >
-            {props.data ? (
-              props.data.map((d, i) => (
-                <div key={`${d.id}-${i}`}>
-                  <div className="card">
-                    <div
-                      className={
-                        currentSlide.toString() == d.id.toString()
-                          ? "card-body highlighted"
-                          : "card-body"
-                      }
-                    >
-                      <img src={d.image} className="card-img-top" alt="..." />
-                      <p className="card-text">{`${d.title}-${currentSlide}`}</p>
-                    </div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <div></div>
-            )}
-          </Carousel>
+          <div class="gallery-controls">
+            <button
+              class="gallery-controls-previous"
+              onClick={() => {
+                setDirectionPrevious((v) => v + 1);
+                setNewDirection("previous");
+              }}
+            >
+              previous
+            </button>
+            <button
+              class="gallery-controls-next"
+              onClick={() => {
+                setDirectionNext((v) => v + 1);
+                setNewDirection("next");
+              }}
+            >
+              next
+            </button>
+          </div>
         </div>
       </div>
     </div>
